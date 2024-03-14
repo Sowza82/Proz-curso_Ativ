b9315afd76f0b5015d836b27b0f15315cda2940c
@Sowza82 <tathyannesg18@gmail.com>
2024-03-13 23:31:19 -0300

    adicionando praatica guiada aula14

---
+++ aula14 joy/script.js
@@ -0,0 +1,9 @@
+const id =41;
+let username='Tay';
+
+if(21 == id && 'Tay' == username) {
+    console.log("Quer alterar sua senha?")
+}  else {
+    console.log("Usuário sem autorização")
+}
+
---
+++ aula14 joy/script2.js
@@ -0,0 +1,30 @@
+//const id =41;
+//let username='Tay';
+//criando um objeto//
+
+let usuario ={
+    id:26,
+    username:'Tay' 
+ }
+
+if(21 == usuario.id || 'Tay' == usuario ['username']){
+    console.log("Quer alterar sua senha?")
+} else {
+    console.log("Usuário sem autorização")
+}
+
+
+let pessoa = {
+    nome:"Nine",
+    idade: 17,
+    altura:1.61,
+    estudante:true,
+    comidaFavorita:["lazanha","pizza","macarronada"],
+    familia:{
+        mae:"Tatiane",
+        pai:"Leandro",
+    }
+
+}
+console.log (pessoa.familia['mae'])
+
